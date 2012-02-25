5aa47b2c82365df4661e128cf22736c7c148a7d6
Matt Baxter <matt@phozop.net>
2012-02-25 00:21:31 -0500

    Skeleton

---
+++ src/to/joe/j2mc/antispam/J2MC_Antispam.java
@@ -0,0 +1,8 @@
+package to.joe.j2mc.antispam;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class J2MC_Antispam extends JavaPlugin implements Listener {
+
+}
